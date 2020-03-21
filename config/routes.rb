c103b73e28c6f724af932a2a4a43508942048a15
kotamori9 <kotamori27@gmail.com>
2020-03-21 20:51:27 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Rails.application.routes.draw do
+  root "tasks#index"
+  devise_for :users
+  # post '/tasks/guest_sign_in', to: 'homes#new_guest'
+
+  
+
+  resources :users, only: [:edit, :update, :destroy]
+  resources :tasks, only: [:index,:new,:create,:show,:destroy] do
+    collection do
+      get 'search'
+    end
+    resources :todos,only: [:index,:new,:create,:destroy] 
+  end
 end
