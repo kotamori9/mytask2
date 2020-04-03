fedeef736f51a96ca7a8daeb017e5e7cffdbc6f9
kotamori9 <kotamori27@gmail.com>
2020-04-03 21:15:04 +0900

    [WIP]バリデーション編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  root "tasks#index"
-  devise_for :users
+  # get 'users/show'
+
+  devise_for :users 
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  
+  root "tasks#index"
+
   resources :users, only: [:edit, :update,:show,:destroy]
+  
   resources :tasks, only: [:index,:new,:create,:show,:destroy] do
     collection do
       get 'search'
