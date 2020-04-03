53ca864e17ce7e45ce87b6cb5531628d7bce7cdb
kotamori9 <kotamori27@gmail.com>
2020-04-03 21:33:32 +0900

    バリデーション変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # get 'users/show'
 
   devise_for :users 
+
+  get 'users/show'
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
