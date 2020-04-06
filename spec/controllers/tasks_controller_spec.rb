7970b5aef49d767a2daff3e345f9e6447f60b7ac
kotamori9 <kotamori27@gmail.com>
2020-04-07 06:01:50 +0900

    [WIP]テストコード

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -6,4 +6,19 @@ describe TasksController do
     it "renders the :new template" do
     end
   end
+
+  # createアクションに関するテスト
+  describe 'Post #create' do
+    let(:params) {user_id: user.id}
+    context '保存に成功した場合' do
+      subject {
+        post :create,
+        params: params
+      }
+      it 'Taskを保存すること' do
+        expect{ subject }.to change(Task, :count).by(1)
+      end
+    end
+  end
+
 end
